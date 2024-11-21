2c0d53ca2632011f7eccc97e0d7caaf6b42d7271
Kishan Singh <136336528+devkishan03@users.noreply.github.com>
2024-11-21 11:33:48 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+ReactDOM.render(
+    <>
+        <h1>This is netflix movie seris list</h1>
+        <p>Favourate movie list</p>
+        <ol>
+            <li>First movie</li>
+            <li>Second movie</li>
+            <li>Third movie</li>
+            <li>Fourth movie</li>
+            <li>Fifth Movie</li>
+
+        </ol>
+    </>,
+    document.getElementById('root')
+)
