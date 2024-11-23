de81635e5b906c50a1143b4561922eabdd7a1f09
Kishan Singh <136336528+devkishan03@users.noreply.github.com>
2024-11-23 17:32:43 +0530

    Add files via upload

---
+++ List.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function OList() {
+  return <ol></ol>;
+}
+function List() {
+  return (
+    <ol>
+      <li>hi</li>
+      <li>my</li>
+      <li>name</li>
+      <li>is</li>
+      <li>kishan</li>
+    </ol>
+  );
+}
+
+export default List;
